984b281f349cdf659d7d504251d9beee5cc84adb
Neythen <zcqsntr@ucl.ac.uk>
2021-02-08 10:03:16 +0000

    commit

--- comb_maths/utilities.py
+++ comb_maths/utilities.py
@@ -7,7 +7,7 @@ def find_unique_lgs(n):
     total = math.factorial(n_IS + 1)
     for i in range(n_IS+1):
         total -= (math.factorial(i)*math.factorial(n_IS-i) - 1)*math.factorial(n_IS)/(math.factorial(n_IS-i)*math.factorial(i))
-
+        print((math.factorial(i)*math.factorial(n_IS-i) - 1)*math.factorial(n_IS)/(math.factorial(n_IS-i)*math.factorial(i)))
     return total
 
 
@@ -19,5 +19,5 @@ def lgs_position(n):
 
 print(find_unique_lgs(2))
 print(lgs_position(2))
-print(find_unique_lgs(3))
-print(find_unique_lgs(4))
+#print(find_unique_lgs(3))
+#print(find_unique_lgs(4))
