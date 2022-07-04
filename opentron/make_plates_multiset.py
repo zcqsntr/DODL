d125827f50147ecda7d12cde5808d4ad740bb082
ajfedorec <ajfedorec@users.noreply.github.com>
2022-07-04 14:46:56 +0100

    implemented experiments that need more than the 4 agar positions available to opentrons by splitting into "set"s of plates

--- opentron/make_plates_multiset.py
+++ opentron/make_plates_multiset.py
@@ -12,7 +12,7 @@ from PIL import Image, ImageDraw
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 
-def get_wells(receiver_coords, inducer_coords, activations):
+def get_logic_combinations(json_data):
     '''
     converts the configuration specified by the coords and activations and creates a list of wells of a 6 well plate where each well will implement each input state
 
@@ -22,40 +22,39 @@ def get_wells(receiver_coords, inducer_coords, activations):
     :param activations:
     :return:
     '''
-    n_inputs = len(inducer_coords)
-    all_inputs = list(map(np.array, list(itertools.product([0, 1], repeat=n_inputs))))
+
+    n_gates = len(json_data)
+
+    all_combinations = []
+    for gate_idx in range(n_gates):
+        json_gate = json_data[gate_idx]
+
+        inducer_coords = np.array(json_gate['IPTG'])
+        n_inputs = len(inducer_coords)
+        gate_input_combinations = list(map(np.array, list(itertools.product([0, 1], repeat=n_inputs))))
+        gate_input_combinations = gate_input_combinations[1:]  # drop first combination as we don't want to create 0,0 for all gates
+
+        bp = np.array(json_gate['ZBD'])
+        th = np.array(json_gate['ZG'])
+
+        for i in range(len(gate_input_combinations)):  # a well in the six well plate for each input state
+            ics = inducer_coords[gate_input_combinations[i] == 1]
+
+            well = {'gate': json_gate['logic_gate'], 'BP': bp.__copy__(), 'TH': th.__copy__(), 'IPTG': ics.__copy__()}
+            all_combinations.append(well)
+    return all_combinations
 
 
-    # get relative (to well origin) opentron coords for the inducers
-
-    wells = []
-
-    print('rc', receiver_coords)
-    bp = receiver_coords[np.array(activations) == 'BP']
-    th = receiver_coords[np.array(activations) == 'TH']
-    for i in range(2**n_inputs): # a well in the six well plate for each input state
-
-
-        ics = inducer_coords[all_inputs[i] == 1]
-
-        well = {'BP':bp, 'TH': th, 'IPTG': ics}
-        wells.append(well)
-    return wells
-
-def convert_to_opentron(coordinates):
+def coord_to_well(coordinate):
     '''
     converts a np array of coordinates into opentron well with format A-P, 1-24
     :param coordinates:
     :return:
     '''
-    if len(coordinates) == 0:
-        return []
-    else:
-        coordinates = np.vstack(coordinates)
-        return [chr(ord('@') + coordinate[0]) + str(coordinate[1]) for coordinate in coordinates]
+    return chr(ord('@') + coordinate[0,0]) + str(coordinate[1,0])
 
 
-def get_plates(wells):
+def get_plates(positions):
     '''
     takes a list of wells an assigns them to opentron positions on a plate
 
@@ -65,38 +64,59 @@ def get_plates(wells):
     :param activations:
     :return:
     '''
-    origins = np.array([[2,1], [2,10], [2, 19], [10, 1], [10, 10], [10, 19]])
+
+    wells_per_plate = 5  # leave final well for blanks
     plates = []
+    ot_plate_positions = ['2', '3', '5', '6']
 
-    n_wells = len(wells)
-    n_plates = math.ceil(n_wells/6)
+    n_wells = len(positions)
+    n_plates_total = math.ceil(n_wells / wells_per_plate)
+    n_ot_positions = len(ot_plate_positions)
+    n_sets = math.ceil(n_plates_total / n_ot_positions)
 
+    for plate_set in range(n_sets):
+        n_plates = min(n_ot_positions, n_plates_total - n_ot_positions * plate_set)
 
-    positions = ['8', '5', '2'] #TODO:: ask ke yan about this
-    for p in range(n_plates):
+        for p in range(n_plates):
+            bandpass_wells = ['L22']
+            threshold_wells = ['N21']
+            IPTG_wells = []
+            first_well = plate_set * n_ot_positions * wells_per_plate + p * wells_per_plate
+            last_well = first_well + wells_per_plate
+            wells_for_plate = positions[first_well:min(last_well, n_wells)]
 
-        bandpass_wells = []
-        threshold_wells = []
-        IPTG_wells = []
+            for w, well in enumerate(wells_for_plate):
+                # converted well position is given by:
+                #   col = col + 9 * (six_well_col - 1)
+                #   if six_well_row == 2:
+                #       row = 17 - row
+                six_well_row = (w // 3) + 1
+                six_well_col = (w % 3) + 1
 
-        wells_for_plate = wells[p*6:min((p+1)*6, n_wells)]
+                well['six_well_coord'] = [six_well_row, six_well_col]
+                well['plate'] = p+1
 
-        for w, well in enumerate(wells_for_plate):
+                well_types = ['BP', 'TH', 'IPTG']
+                for wtype in well_types:
+                    if well[wtype].size > 1:   #TODO: for some reason empty well are given a size == 1
+                        for bp_well in well[wtype]:
+                            bp_well[1] = bp_well[1] + 9 * (six_well_col - 1)
+                            if six_well_row == 2:
+                                bp_well[0] = 17 - bp_well[0]
 
-            if not well['BP'].size == 0:
-                bandpass_wells.append(well['BP'] + origins[w])# - np.array([1,1])) #- np.array([1,1]) to acocunt for 1 indexing
-            if not well['TH'].size == 0:
-                threshold_wells.append(well['TH'] + origins[w])#- np.array([1,1]))
+                            ot_well = coord_to_well(bp_well)
+                            if wtype == 'BP':
+                                bandpass_wells.append(ot_well)
+                            elif wtype == 'TH':
+                                threshold_wells.append(ot_well)
+                            elif wtype == 'IPTG':
+                                IPTG_wells.append(ot_well)
 
-            if not well['IPTG'].size == 0:
-                IPTG_wells.append(well['IPTG'] + origins[w])#- np.array([1,1]))
+            plate = {'set': plate_set, 'position': ot_plate_positions[p], 'BP': bandpass_wells, 'TH': threshold_wells, 'IPTG': IPTG_wells}
 
-        bandpass_wells, threshold_wells, IPTG_wells = map(convert_to_opentron, [bandpass_wells, threshold_wells, IPTG_wells])
-
-        plate = {'position': positions[p], 'BP':bandpass_wells, 'TH':threshold_wells, 'IPTG': IPTG_wells}
-
-        plates.append(plate)
+            plates.append(plate)
     return plates
+
 
 def draw_blank_plate(scale):
     plate_dim = np.array([24, 16])
@@ -126,6 +146,7 @@ def draw_blank_plate(scale):
 
     return im
 
+
 def draw_wells(plate_draw, wells, colour, scale):
 
     for well in wells:
@@ -133,6 +154,7 @@ def draw_wells(plate_draw, wells, colour, scale):
         y = int(ord(well[0]) - ord('@'))
         plate_draw.rectangle(((x * scale +scale/10, y * scale +scale/10), ((x + 1) * scale-scale/10, (y + 1) * scale-scale/10)),
                        fill=colour, outline=(0, 0, 0), width=2)
+
 
 def draw_plates(plates, out_dir):
     scale = 20
@@ -143,8 +165,6 @@ def draw_plates(plates, out_dir):
         draw_wells(draw, plate['BP'], (187, 161, 205), 20)
         draw_wells(draw, plate['TH'], (59, 140, 53), 20)
         draw_wells(draw, plate['IPTG'], (55, 73, 153), 20)
-
-
 
         im.save(os.path.join(out_dir, 'plate{}.png'.format(p)))
 
@@ -168,19 +188,11 @@ if __name__ == '__main__':
 
     os.makedirs(out_file, exist_ok=True)
 
-
-
-
-
     data = json.load(open(in_file))
-    receiver_inds = np.array(data['receiver_inds'])
-    IPTG_inds = np.array(data['IPTG_inds'])
-    activations = data['activations']
 
-    print(receiver_inds, IPTG_inds, activations)
-    wells = get_wells(receiver_inds, IPTG_inds, activations)
-    print(wells)
-    plates = get_plates(wells)
+    combinations = get_logic_combinations(data)
+    print(combinations)
+    plates = get_plates(combinations)
 
     if args.plot:
         draw_plates(plates,  out_file)
---
+++ opentron/run_script_multiset.py
@@ -0,0 +1,120 @@
+import json
+import os
+metadata = {'apiLevel': '2.0'}
+
+waste_destination = [  # COMMENT OUT THE BLOCKS NOT IN USE
+    # 'A3', 'A4',                                 # A1
+    # 'B2', 'B3', 'B4', 'B5',
+    # 'C1', 'C2', 'C3', 'C4', 'C5', 'C6',
+    # 'D1', 'D2', 'D3', 'D4', 'D5', 'D6',
+    # 'E1', 'E2', 'E3', 'E4', 'E5', 'E6',
+    # 'F1', 'F2', 'F3', 'F4', 'F5', 'F6',
+    # 'G2', 'G3', 'G4', 'G5',
+    'A12', 'A13',                               # A2
+    'B11', 'B12', 'B13', 'B14',
+    'C10', 'C11', 'C12', 'C13', 'C14', 'C15',
+    'D10', 'D11', 'D12', 'D13', 'D14', 'D15',
+    'E10', 'E11', 'E12', 'E13', 'E14', 'E15',
+    'F10', 'F11', 'F12', 'F13', 'F14', 'F15',
+    'G11', 'G12', 'G13', 'G14',
+    # 'A21', 'A22',                               # A3
+    # 'B20', 'B21', 'B22', 'B23',
+    # 'C19', 'C20', 'C21', 'C22', 'C23', 'C24',
+    # 'D19', 'D20', 'D21', 'D22', 'D23', 'D24',
+    # 'E19', 'E20', 'E21', 'E22', 'E23', 'E24',
+    # 'F19', 'F20', 'F21', 'F22', 'F23', 'F24',
+    # 'G20', 'G21', 'G22', 'G23',
+    # 'J2', 'J3', 'J4', 'J5',                     # B1
+    # 'K1', 'K2', 'K3', 'K4', 'K5', 'K6',
+    # 'L1', 'L2', 'L3', 'L4', 'L5', 'L6',
+    # 'M1', 'M2', 'M3', 'M4', 'M5', 'M6',
+    # 'N1', 'N2', 'N3', 'N4', 'N5', 'N6',
+    # 'O2', 'O3', 'O4', 'O5',
+    # 'P3', 'P4',
+    # 'J11', 'J12', 'J13', 'J14',                 # B2
+    # 'K10', 'K11', 'K12', 'K13', 'K14', 'K15',
+    # 'L10', 'L11', 'L12', 'L13', 'L14', 'L15',
+    # 'M10', 'M11', 'M12', 'M13', 'M14', 'M15',
+    # 'N10', 'N11', 'N12', 'N13', 'N14', 'N15',
+    # 'O11', 'O12', 'O13', 'O14',
+    # 'P12', 'P13',
+    # 'J20', 'J21', 'J22', 'J23',                 # B3
+    # 'K19', 'K20', 'K21', 'K22', 'K23', 'K24',
+    # 'L19', 'L20', 'L21', 'L22', 'L23', 'L24',
+    # 'M19', 'M20', 'M21', 'M22', 'M23', 'M24',
+    # 'N19', 'N20', 'N21', 'N22', 'N23', 'N24',
+    # 'O20', 'O21', 'O22', 'O23',
+    # 'P21', 'P22'
+]
+
+
+def pipette(dest_plates, liquid_type, liquid_source, pip, waste_plate, waste_counter, plate_set):
+    # atm using pipette with max volume 10uL and pipetting 1uL each time, will need to change if these change
+    max_vol = 10
+    vol_disp = 1
+
+    dispense_count = 0
+
+    pip.pick_up_tip()
+    pip.aspirate(max_vol, liquid_source.bottom(), rate=0.5)
+    pip.touch_tip()
+
+    for plate in dest_plates:
+        if plate['set'] == plate_set:
+            wells = plate[liquid_type]
+            agar = plate['agar']
+
+            for well in wells:
+                dispense_count += 1
+
+                # discard the last few drops and re-aspirate
+                if dispense_count > 7:
+                    pip.blow_out(liquid_source)
+                    pip.aspirate(max_vol, liquid_source.bottom(), rate=0.5)
+                    pip.touch_tip()
+                    dispense_count = 1
+
+                # discard first drop to waste agar
+                if dispense_count == 1:
+                    pip.dispense(vol_disp, waste_plate[waste_destination[waste_counter[0]]].bottom(3), rate=0.5)
+                    waste_counter[0] += 1  # increment waste position counter
+                    dispense_count += 1
+
+                pip.dispense(1, agar[well].bottom(3), rate=0.5)
+
+    pip.drop_tip()
+
+
+def run(protocol):
+
+    destination_plates = json.load(open(os.path.join('output/plate_config.json')))
+    deep96 = protocol.load_labware('nest_96_wellplate_2ml_deep', '4')
+    waste_agar = protocol.load_labware('corning_384_wellplate_112ul_flat', '8')
+    tip10 = protocol.load_labware('opentrons_96_filtertiprack_10ul', '1')
+
+    p10 = protocol.load_instrument('p10_single',
+                                   mount='left',
+                                   tip_racks=[tip10])
+
+    # THINGS TO CHANGE AT THE BEGINNING OF AN EXPERIMENT
+    bp_source = deep96['A1']  # bandpass
+    th_source = deep96['B1']  # threshold
+    iptg_source = deep96['C1']  # 7.5mM IPTG
+    p10.starting_tip = tip10.well('B10')
+    plate_set = 1
+
+    waste_position_counter = [0]  # this is a list to allow pass by reference
+
+    for plate in destination_plates:
+        if plate['set'] == plate_set:
+            plate['agar'] = protocol.load_labware('corning_384_wellplate_112ul_flat', plate['position'])
+
+    # do the bandpass receivers
+    pipette(destination_plates, 'BP', bp_source, p10, waste_agar, waste_position_counter, plate_set)
+
+    # do the threshold receivers
+    pipette(destination_plates, 'TH', th_source, p10, waste_agar, waste_position_counter, plate_set)
+
+    # do the IPTG
+    pipette(destination_plates, 'IPTG', iptg_source, p10, waste_agar, waste_position_counter, plate_set)
+
